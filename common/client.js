88c7cd4ad634ca635de48969e6c207f9712643d1
Zeuxis Lo <seekstudio@gmail.com>
2020-05-13 16:50:42 +0800

    make the endpoint configurable

--- common/client.js
+++ common/client.js
@@ -1,10 +1,11 @@
 const got = require("got");
 const { CookieJar } = require("tough-cookie");
+const settings = require("../settings.json");
 
 const cookieJar = new CookieJar();
 
 const client = got.extend({
-    prefixUrl: "http://h.e-talking.net/",
+    prefixUrl: settings.endpoint,
     cookieJar: cookieJar,
     headers  : {
         "user-agent": "hof-bot"
